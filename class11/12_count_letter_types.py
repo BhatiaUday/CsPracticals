dc32446e46ef2809d61c8877923894abc888917c
BhatiaUday <101352755+BhatiaUday@users.noreply.github.com>
2022-10-30 15:00:12 +0530

    e

--- class11/12_count_letter_types.py
+++ class11/12_count_letter_types.py
@@ -1,10 +1,10 @@
-str=input("Enter String: ")
+string=input("Enter String: ")
 
 vowels = 0
 consonant = 0
 specialChar = 0
 digit = 0
-for i in range(0, len(str)):
+for i in range(0, len(string)):
 	ch = str[i]
 	if ch.isalpha():
 		if (ch == 'a' or ch == 'e' or ch == 'i'or ch == 'o' or ch == 'u'):
--- class12/28_read_file_list_letter_types.py
+++ class12/28_read_file_list_letter_types.py
@@ -1,14 +1,11 @@
 f=open("file.txt","r")
 
-
-str=f.read()
-
-
+string=f.read()
 vowels = 0
 consonant = 0
 specialChar = 0
 digit = 0
-for i in range(0, len(str)):
+for i in range(0, len(string)):
 	ch = str[i]
 	if ch.isalpha():
 		if (ch == 'a' or ch == 'e' or ch == 'i'or ch == 'o' or ch == 'u'):
